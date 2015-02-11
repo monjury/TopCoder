cb95de8e5c66b19833d6b6e4d7a730a392e32368
samchandra <samchandra@ebay.com>
2015-02-10 20:11:12 -0800

    SRM 441

--- differentstrings-java/DifferentStrings.java
+++ differentstrings-java/DifferentStrings.java
@@ -2,19 +2,16 @@
 public class DifferentStrings {
 
 	public int minimize(String A, String B) {
-		if (A.length() < B.length()) {
-			int minDiff = A.length();
-			int lengthDiff = B.length() - A.length();
-			for (int i = 0; i <= lengthDiff; i++) {
-				String comparable = B.substring(i, i + A.length());
-				int diff = difference(A, comparable);
-				if (diff < minDiff) {
-					minDiff = diff;
-				}
+		int minDiff = A.length();
+		int lengthDiff = B.length() - A.length();
+		for (int i = 0; i <= lengthDiff; i++) {
+			String comparable = B.substring(i, i + A.length());
+			int diff = difference(A, comparable);
+			if (diff < minDiff) {
+				minDiff = diff;
 			}
-			return minDiff;
 		}
-		return difference(A, B);
+		return minDiff;
 	}
 
 	private int difference(String A, String B) {
